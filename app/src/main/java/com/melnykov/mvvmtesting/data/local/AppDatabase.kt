860a3fe3db339c11c7f51d26ac96fed3eebadd10
Oleksandr Melnykov <makovkastar@gmail.com>
2018-12-17 15:53:53 +0100

    Disable database schema export

--- app/src/main/java/com/melnykov/mvvmtesting/data/local/AppDatabase.kt
+++ app/src/main/java/com/melnykov/mvvmtesting/data/local/AppDatabase.kt
@@ -5,7 +5,7 @@ import android.arch.persistence.room.RoomDatabase
 import com.melnykov.mvvmtesting.data.local.dao.UserDao
 import com.melnykov.mvvmtesting.data.model.User
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 }
